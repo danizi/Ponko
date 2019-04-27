23c6fb5dce32f232c4381067874fba4e1114d5bb
xm <15074770708@163.com>
2019-04-28 01:39:00 +0800

    first commit

--- app/src/main/java/com/ponko/cn/module/login/LoginStartAct.kt
+++ app/src/main/java/com/ponko/cn/module/login/LoginStartAct.kt
@@ -4,10 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.ponko.cn.R
 
-class StartLoginAct : AppCompatActivity() {
+class LoginStartAct : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_login)
+        setContentView(R.layout.activity_login_start)
     }
 }
