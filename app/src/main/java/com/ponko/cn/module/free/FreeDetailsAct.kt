3dc25df09582a595e477c4137fc7cb49f8a7d346
xm <15074770708@163.com>
2019-04-30 01:15:54 +0800

    first commit

---
+++ app/src/main/java/com/ponko/cn/module/free/FreeDetailsAct.kt
@@ -0,0 +1,13 @@
+package com.ponko.cn.module.free
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ponko.cn.R
+
+class FreeDetailsAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_free_details)
+    }
+}
